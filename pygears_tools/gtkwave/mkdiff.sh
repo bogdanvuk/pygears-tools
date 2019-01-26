fcbd295debf34e1d8771ae5ff8ae8155a52d1b8d
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2019-01-26 12:47:01 +0100

    New version of gtkwave that supports live VCD with TCL interface

--- pygears_tools/gtkwave/mkdiff.sh
+++ pygears_tools/gtkwave/mkdiff.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/tcl_commands.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_ch/src/tcl_commands.c > tcl_commands.diff
+diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/tcl_commands.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/tcl_commands.c > tcl_commands.diff
 
-diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/main.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_ch/src/main.c > main.diff
+diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/vcd_partial.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/vcd_partial.c > vcd_partial.diff
 
-diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/signalwindow.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_ch/src/signalwindow.c > signalwindow.diff
+diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/ttranslate.h /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/ttranslate.h > ttranslate.diff
+
+diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/main.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/main.c > main.diff
+
+diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/signalwindow.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/signalwindow.c > signalwindow.diff
+
+diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/helpers/shmidcat.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/helpers/shmidcat.c > shmidcat.diff
