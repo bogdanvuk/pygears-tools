177c56aa4f7f2c6f7522e865162f651bb476b33c
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2019-01-29 00:29:11 +0100

    Add proc to find last value of traces for gtkwave

--- pygears_tools/gtkwave/mkdiff.sh
+++ pygears_tools/gtkwave/mkdiff.sh
@@ -2,6 +2,10 @@
 
 diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/tcl_commands.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/tcl_commands.c > tcl_commands.diff
 
+diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/tcl_support_commands.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/tcl_support_commands.c > tcl_support_commands.diff
+
+diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/tcl_support_commands.h /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/tcl_support_commands.h > tcl_support_commands_h.diff
+
 diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/vcd_partial.c /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/vcd_partial.c > vcd_partial.diff
 
 diff /tools/gtkwave/_install/gtkwave-gtk3-3.3.98_orig/src/ttranslate.h /tools/gtkwave/_install/gtkwave-gtk3-3.3.98/src/ttranslate.h > ttranslate.diff
