39f1f4340635a3e14db2148738dbc156c9005a9b
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2020-10-02 16:25:41 +0200

    Add missing git dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygears-tools',
-    version='0.1.8',
+    version='0.1.13',
     description='Helper scripts for installing pygears related tools',
 
     # The project's main homepage.
